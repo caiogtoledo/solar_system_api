d261f896c4529f880f79a19b83aec1cff59b398b
Caio Toledo <caiobbgtoledo@gmail.com>
2024-08-01 02:21:19 +0000

    testing measure battery controller

--- tests/modules/create_user/app/test_measure_battery_controller.py
+++ tests/modules/create_user/app/test_measure_battery_controller.py
@@ -4,7 +4,7 @@ from src.shared.helpers.external_interfaces.http_models import HttpRequest
 from src.shared.infra.repositories.battery_repository_mock import BatteryRepositoryMock
 
 
-class Test_CreateUserControler:
+class Test_MeasureBatteryControler:
     def test_measurement_battery_controller(self):
         repo = BatteryRepositoryMock()
         usecase = MeasureBatteryUsecase(repo=repo)
@@ -34,12 +34,17 @@ class Test_CreateUserControler:
         controller = MeasureBatteryController(usecase=usecase)
 
         request = HttpRequest(body={
-            'email': '21.01444-2@maua.br'})
+            # 'battery_id': '1',
+            'soc': 0.5,
+            'voltage': 3.7,
+            'current': 0.1,
+            'temperature': 25.0
+        })
 
         response = controller(request=request)
 
         assert response.status_code == 400
-        assert response.body == "Field name is missing"
+        assert response.body == "Field battery_id is missing"
 
     def test_create_user_controller_missing_soc(self):
         repo = BatteryRepositoryMock()
@@ -47,37 +52,158 @@ class Test_CreateUserControler:
         controller = MeasureBatteryController(usecase=usecase)
 
         request = HttpRequest(body={
-            'name': 'Branco do Branco Branco da Silva'})
+            'battery_id': "1",
+            # 'soc': 0.5,
+            'voltage': 3.7,
+            'current': 0.1,
+            'temperature': 25.0
+        })
 
         response = controller(request=request)
 
         assert response.status_code == 400
-        assert response.body == "Field email is missing"
+        assert response.body == "Field soc is missing"
 
-    def test_create_user_controller_invalid_email(self):
-        repo = UserRepositoryMock()
-        usecase = CreateUserUsecase(repo=repo)
-        controller = CreateUserController(usecase=usecase)
+    def test_create_user_controller_missing_voltage(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
 
         request = HttpRequest(body={
-            'name': 'Branco do Branco Branco da Silva',
-            'email': 'branco@branco'})
+            'battery_id': "1",
+            'soc': 0.5,
+            # 'voltage': 3.7,
+            'current': 0.1,
+            'temperature': 25.0
+        })
 
         response = controller(request=request)
 
         assert response.status_code == 400
-        assert response.body == "Field email is not valid"
+        assert response.body == "Field voltage is missing"
 
-    def test_create_user_controller_invalid_name(self):
-        repo = UserRepositoryMock()
-        usecase = CreateUserUsecase(repo=repo)
-        controller = CreateUserController(usecase=usecase)
+    def test_create_user_controller_missing_current(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
 
         request = HttpRequest(body={
-            'name': 'B',
-            'email': 'branco@branco.com'})
+            'battery_id': "1",
+            'soc': 0.5,
+            'voltage': 3.7,
+            # 'current': 0.1,
+            'temperature': 25.0
+        })
 
         response = controller(request=request)
 
         assert response.status_code == 400
-        assert response.body == "Field name is not valid"
+        assert response.body == "Field current is missing"
+
+    def test_create_user_controller_missing_temperature(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': "1",
+            'soc': 0.5,
+            'voltage': 3.7,
+            'current': 0.1,
+            # 'temperature': 25.0
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field temperature is missing"
+
+    def test_create_user_controller_invalid_battery_id(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': 1,
+            'soc': 0.5,
+            'voltage': 3.7,
+            'current': 0.1,
+            'temperature': 25.0
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field battery_id is not valid"
+
+    def test_create_user_controller_invalid_soc(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': "1",
+            'soc': "0.5 V",
+            'voltage': 3.7,
+            'current': 0.1,
+            'temperature': 25.0
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field soc is not valid"
+
+    def test_create_user_controller_invalid_voltage(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': "1",
+            'soc': 0.5,
+            'voltage': "3.7 v",
+            'current': 0.1,
+            'temperature': 25.0
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field voltage is not valid"
+
+    def test_create_user_controller_invalid_current(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': "1",
+            'soc': 0.5,
+            'voltage': 3.7,
+            'current': ["0.1"],
+            'temperature': 25.0
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field current is not valid"
+
+    def test_create_user_controller_invalid_temperature(self):
+        repo = BatteryRepositoryMock()
+        usecase = MeasureBatteryUsecase(repo=repo)
+        controller = MeasureBatteryController(usecase=usecase)
+
+        request = HttpRequest(body={
+            'battery_id': "1",
+            'soc': 0.5,
+            'voltage': 3.7,
+            'current': 0.1,
+            'temperature': "25.0 Celsius"
+        })
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field temperature is not valid"
