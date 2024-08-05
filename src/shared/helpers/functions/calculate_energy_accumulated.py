4a542f887292aa973b9f603d64312e25c857b70d
Caio Toledo <caiobbgtoledo@gmail.com>
2024-08-05 00:04:21 +0000

    function to calculate energy accumulated

---
+++ src/shared/helpers/functions/calculate_energy_accumulated.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+def calculate_energy_accumulated(prev_energy, instantly_power, prev_timestamp, current_timestamp):
+    """
+    A energia acumulada (E) é a integral da potência (P) ao longo do tempo (t). 
+    Para simplificar, você pode aproximar a integral somando a potência instantânea 
+    multiplicada pelo intervalo de tempo (Δt) entre as leituras.
+    """
+
+    prev_time = datetime.datetime.fromtimestamp(prev_timestamp)
+    current_time = datetime.datetime.fromtimestamp(current_timestamp)
+
+    time_diff = (current_time - prev_time).total_seconds() / 3600.0
+
+    energy_accumulated = prev_energy + (instantly_power * time_diff)
+
+    return energy_accumulated
