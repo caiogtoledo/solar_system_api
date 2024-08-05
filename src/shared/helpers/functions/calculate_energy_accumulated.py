d3f6bcc8e3c3febbfbaad15a3fa6c2cb411c1d76
Caio Toledo <caiobbgtoledo@gmail.com>
2024-08-05 01:05:55 +0000

    try except for errors

--- src/shared/helpers/functions/calculate_energy_accumulated.py
+++ src/shared/helpers/functions/calculate_energy_accumulated.py
@@ -6,13 +6,17 @@ def calculate_energy_accumulated(prev_energy, instantly_power, prev_timestamp, c
     A energia acumulada (E) é a integral da potência (P) ao longo do tempo (t). 
     Para simplificar, você pode aproximar a integral somando a potência instantânea 
     multiplicada pelo intervalo de tempo (Δt) entre as leituras.
+
+    VERIFICAR CASOS DE DESLIGAMENTO E LIGAMENTO DO PAINEL SOLAR E OUTROS EQUIPAMENTOS CONSUMIDORES
     """
+    try:
+        prev_time = datetime.datetime.fromtimestamp(prev_timestamp)
+        current_time = datetime.datetime.fromtimestamp(current_timestamp)
 
-    prev_time = datetime.datetime.fromtimestamp(prev_timestamp)
-    current_time = datetime.datetime.fromtimestamp(current_timestamp)
+        time_diff = (current_time - prev_time).total_seconds() / 3600.0
 
-    time_diff = (current_time - prev_time).total_seconds() / 3600.0
+        energy_accumulated = prev_energy + (instantly_power * time_diff)
 
-    energy_accumulated = prev_energy + (instantly_power * time_diff)
-
-    return energy_accumulated
+        return energy_accumulated
+    except Exception as e:
+        raise e
