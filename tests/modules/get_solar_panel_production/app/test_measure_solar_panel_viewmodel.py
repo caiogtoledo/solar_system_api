b1d62e287734344a573ec59b21fddf399ef8eb9b
Caio Toledo <caiobbgtoledo@gmail.com>
2024-08-11 01:23:43 +0000

    get solar panel production tests

---
+++ tests/modules/get_solar_panel_production/app/test_measure_solar_panel_viewmodel.py
@@ -0,0 +1,30 @@
+
+import datetime
+from src.modules.get_solar_panel_production.app.get_solar_panel_production_viewmodel import GetSolarPanelProductionViewmodel
+from src.shared.domain.entities.solar_panel import SolarPanel
+
+
+class Test_GetSolarPanelProductionViewModel:
+
+    def test_get_solar_panel_production_viewmodel(self):
+        test_id = "1"
+        solar_panel_measurement = SolarPanel(
+            solar_panel_id=test_id,
+            instantly=0.5,
+            daily=3.7,
+            monthly=317.3,
+            timestamp=int(datetime.datetime.now().timestamp()),
+        )
+        userViewmodel = GetSolarPanelProductionViewmodel(
+            solar_panel=solar_panel_measurement).to_dict()
+
+        expected = {
+            'solar_panel_id': "1",
+            'instantly': 0.5,
+            'daily': 3.7,
+            'monthly': 317.3,
+            'timestamp': int(datetime.datetime.now().timestamp()),
+            'message': f"this is the last measure of the solar panel: {test_id}"
+        }
+
+        assert expected == userViewmodel
